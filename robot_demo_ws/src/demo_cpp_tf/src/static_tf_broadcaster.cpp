7682fa3fb478df41636219097c068372b56b81bc
Edward-Elric-5553 <edward.elric.5553@gmail.com>
2025-07-24 14:32:03 +0800

    create demo tf package,and update static tf publisher node.

---
+++ robot_demo_ws/src/demo_cpp_tf/src/static_tf_broadcaster.cpp
@@ -0,0 +1,42 @@
+#include <memory>
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+
+class StaticTFBroadCaster : public rclcpp::Node{
+    private:
+        std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
+
+    public:
+        StaticTFBroadCaster():Node("tf_broadcaster_node"){
+            tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+            this->publish_tf();
+        }        
+
+        void publish_tf(){
+            geometry_msgs::msg::TransformStamped transform;
+            transform.header.stamp = this->get_clock()->now();
+            transform.header.frame_id = "map";
+            transform.child_frame_id = "target_point";
+            transform.transform.translation.x = 5.0;
+            transform.transform.translation.y = 3.0;
+            transform.transform.translation.z = 0.0;
+            tf2::Quaternion quat;
+            quat.setRPY(0, 0, 60 * M_PI / 180);
+            transform.transform.rotation = tf2::toMsg(quat);
+            tf_broadcaster_->sendTransform(transform);
+        }
+
+};
+
+
+int main(int argc, char* argv[]){
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<StaticTFBroadCaster>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
